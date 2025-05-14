fef5d668d0951ab8079142999d6faa5a6abccaac
Sovch <valentinafediakova@gmail.com>
2025-05-14 23:04:57 +0400

    mobile layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,29 @@
-import './App.scss'
+import { useState } from 'react'
 import { AddTodo } from './components/AddTodo/AddTodo'
 import { FilterTodo } from './components/FilterTodo/FilterTodo'
 import { Todolist } from './components/Todolist/Todolist'
 
+import './App.scss'
+
 function App() {
+  const [isOpenMenu, setIsOpenMenu] = useState(false)
+  
+  const handleToggleMenu = () => {
+    setIsOpenMenu(!isOpenMenu)
+    
+  }
 
   return (
     <div className='app-container'>
       <div className="app-wrap">
         <h1 className="app-title">Todo List</h1>
+        <div className='app-mobile-menu' onClick={handleToggleMenu}></div>
         <div className='app-content'>
           <div className='app-todos'>
             <AddTodo/>
             <Todolist />
           </div>
-          <FilterTodo />
+          <FilterTodo isOpenMenu={isOpenMenu} onHandleToggleMenu={handleToggleMenu}/>
         </div>
       </div>
     </div>
--- src/components/FilterTodo/FilterTodo.tsx
+++ src/components/FilterTodo/FilterTodo.tsx
@@ -6,16 +6,21 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import './FilterTodo.scss';
 
+interface FilterTodoProps {
+  isOpenMenu: boolean;
+  onHandleToggleMenu: () => void;
+}
 
-export const FilterTodo = () => {
+export const FilterTodo: React.FC<FilterTodoProps> = ({ isOpenMenu, onHandleToggleMenu }) => {
   const dispatch = useDispatch()
   const currentFilter = useSelector((state: RootState) => state.todos.filter)
   const handleFilterChange = (filter: Filter) => {
     dispatch(setFilter(filter));
+    onHandleToggleMenu();
   };
 
   return (
-    <div className="filter-todo__content">
+    <div className={`filter-todo__content ${isOpenMenu ? 'filter-todo__content_open' : 'filter-todo__content_close'}`}>
       <div className='filter-todo__wrap'>
         {filters.map((filter) => (
           <Button isActive={currentFilter === filter} onHandleClick={() => handleFilterChange(filter)} text={filter.charAt(0).toUpperCase() + filter.slice(1)}/>
